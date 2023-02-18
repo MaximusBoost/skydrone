6ffb3062258a27a9f0ddb93fb44897e6da051a2b
max7891112 <max.alkhimovich@gmail.com>
2023-02-18 08:51:08 +0700

    swiper in dev\

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-import Swiper from 'swiper';
-// import 'swiper/scss';
-const swiper = new Swiper('.image-slider')
+import Swiper, {Navigation, Pagination, Autoplay, EffectFade} from 'swiper';
+Swiper.use([Navigation, Pagination, Autoplay, EffectFade]);
+new Swiper('.image-slider', {
+    modules: [Navigation,Pagination],
+    slidesPerView: 'auto',
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    autoHeight: true,
+    loop: true,
+    speed: 2500,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true,
+    },
+})
