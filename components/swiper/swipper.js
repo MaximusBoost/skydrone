5ae1c55ee3cc5c5d0ca84acbf5161c2e339b8285
max7891112 <max.alkhimovich@gmail.com>
2023-02-19 10:09:23 +0700

    work swiper

--- components/swiper/swipper.js
+++ components/swiper/swipper.js
@@ -1,7 +1,30 @@
-import Swiper from "swiper";
+import Swiper, {Navigation, Pagination, Autoplay, EffectFade} from 'swiper';
+Swiper.use([Navigation, Pagination, Autoplay, EffectFade]);
 class mySwiper {
-    addPagination() {
-        
+    addPagination() {     
+        let swiper = new Swiper('.image-slider', {
+            modules: [Navigation,Pagination],
+            slidesPerView: 'auto',
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev'
+            },
+            autoHeight: true,
+            loop: true,
+            speed: 500,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true
+            }
+        })
+
+        let links = document.querySelectorAll('.navigation-menu__link')
+        for(let i = 0; i < links.length; i++) {
+            links[i].addEventListener('click', function() {
+            swiper.slideTo(i,300,true);
+            return false;
+            });
+        };
     };
 };
 
